18a9a2baa051feab003c4a4eaf894cc0633d7561
Shelby Spees <shelby@honeycomb.io>
2020-08-29 23:42:32 -0700

    Implement Hugo thread embed generator

---
+++ main.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    bearer_token: process.env.TWITTER_BEARER_TOKEN
+});
+
+var params = {};
+var tweets = [];  // in reverse order
+
+var get_prev = function (tweetID) {
+    client.get('statuses/show/' + tweetID, params, function (error, current, response) {
+        if (!error) {
+            tweets.push(current);
+            if (current.in_reply_to_screen_name == current.user.screen_name) {
+                get_prev(current.in_reply_to_status_id_str);
+            }
+            else {
+                while (tweets.length > 0) {
+                    console.log(`{{< tweet-single ${tweets.pop().id_str} >}}`);
+                }
+            }
+        }
+        else {
+            console.log(error);
+        }
+    });
+}
+
+get_prev(process.argv[2]);
